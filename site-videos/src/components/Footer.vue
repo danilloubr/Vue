779be5c328e5b5dfe64e0fc392ca02a2c5fe81d8
Danilo Sousa <danillou@hotmail.com>
2022-02-03 23:27:59 -0300

    First call of api get github/wdev

--- site-videos/src/components/Footer.vue
+++ site-videos/src/components/Footer.vue
@@ -1,10 +1,31 @@
 <template>
-  <footer></footer>
+  <footer>
+    <a
+      class="social-link"
+      :href="item.link"
+      v-for="(item, index) in socialLinks"
+      :key="index"
+    >
+      <img :src="item.icon" :alt="item.label" />
+    </a>
+  </footer>
 </template>
 
 <script>
+import api from "../services/api.js";
+
 export default {
   name: "Footer",
+  data() {
+    return {
+      socialLinks: [],
+    };
+  },
+  async mounted() {
+    const resp = await api.get("/social-links.json");
+    const { data } = resp;
+    this.socialLinks = data;
+  },
 };
 </script>
 
@@ -13,5 +34,19 @@ footer {
   width: 100%;
   height: 60px;
   background-color: var(--color-background-nav);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.social-link {
+  border: 2px solid var(--color-text-light);
+  border-radius: 50%;
+  padding: 5px;
+  margin: 0px 10px;
+}
+
+.social-link img {
+  width: 20px;
 }
 </style>
